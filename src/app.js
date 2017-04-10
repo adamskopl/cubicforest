92efec2e36029e4c200775d407d56f8b53c7c496
adam.skobo <adam@skobo.pl>
2017-04-11 00:21:03 +0200

    removing test files

--- src/app.js
+++ src/app.js
@@ -1,17 +1,1 @@
-import testVal from './testModule';
-
-const d = 10;
-console.log(testVal.a());
-
-const a = new WeakMap();
-const obj = {};
-a.set(obj, 'objxxx');
-console.log(a.get(obj));
-
-const p = new Promise(function(ok) {
-  ok('wow');
-});
-
-p.then(a => {
-  console.log(a);
-});
+console.log('app');
